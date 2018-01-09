5857034f7357b7f9db4772d625f6ca3cf27a670d
nicolas <nleyssenne@itlink.fr>
2018-01-09 08:01:34 +0100

    re;ove test3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel TEST3
+                    Laravel
+                    
                 </div>
 
                 <div class="links">
