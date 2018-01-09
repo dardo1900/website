f23b062f9254069be5875e16d310e9915c014d79
nicolas <nleyssenne@itlink.fr>
2018-01-09 07:58:28 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel TEST2
+                    Laravel TEST3
                 </div>
 
                 <div class="links">
